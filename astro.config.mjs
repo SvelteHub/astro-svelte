15043a73e95890375961bf3dceb432e64601fc08
Jerric Lyns John <jerriclynsjohn@outlook.com>
2023-10-26 22:19:25 +0530

    fix: changing deployment to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind({ applyBaseStyles: false })],
+  integrations: [
+    svelte(),
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
   output: 'server',
-  adapter: vercel()
+  adapter: cloudflare()
 });
